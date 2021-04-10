9402051b82fe0ed69b189303e1fc7502dbcb98d6
azizulsoftsw <azizul.softsw@gmail.com>
2021-04-10 22:15:01 +0600

    Update main.js

--- themes/sedge/assets/js/main.js
+++ themes/sedge/assets/js/main.js
@@ -281,7 +281,104 @@ if(windowWidth > 768){
     });
 }
 
+$("#register_next").prop("disabled",true);
+$("#reg_email").bind('blur keyup change click', function(){
+  if(isValidEmailAddress($(this).val())){
+    $(this).css({"border": "0px solid red", "color": "#000"});
+    $("#register_next").prop("disabled",false);
+  }else{
+    $(this).css({"border": "1px solid red", "color": "red"});
+    $("#register_next").prop("disabled",true);
+  }
+})
 
+$("#for_business").on('change', function(){
+var html = '<p class="form-row form-row-wide" id="billing_company_field">' +
+        '<label for="billing_company" class="">Bedrijfsnaam</label>' +
+        '<span class="woocommerce-input-wrapper">' +
+          '<input type="text" class="input-text " name="billing_company" id="billing_company" placeholder="Bedrijfsnaam">' +
+        '</span>' +
+      '</p>' +
+      '<p class="form-row form-row-wide" id="billing_btw_nummer_field">' +
+        '<label for="billing_btw_nummer" class="">BTW-nummer</label>' +
+        '<span class="woocommerce-input-wrapper">' +
+          '<input type="text" class="input-text " name="billing_btw_nummer" id="billing_btw_nummer" placeholder="BTW-nummer" required>' +
+        '</span>' +
+      '</p>'+
+      '<p class="form-row form-row-wide" id="billing_btw_nummer_field">' +
+        '<label for="billing_reference" class="">Referentie</label>' +
+        '<span class="woocommerce-input-wrapper">' +
+          '<input type="text" class="input-text " name="billing_reference" id="billing_reference" placeholder="Referentie" required>' +
+        '</span>' +
+      '</p>';
+
+  $("#extra_fields").html(html);
+});
+$("#private").on('change', function(){
+
+var html = '';
+  $("#extra_fields").empty(html);
+})
+
+jQuery('body').on('wc_cart_emptied', function(){
+  location.reload();
+  //console.log('wc_cart_emptied triggered');
+});
+
+// Registration form validation
+$("#re_password").bind('blur keyup change', function(){
+  $("#register_action_btn").attr('disabled','disabled');
+  var pass = $('#re_password').val();
+  //check the strings
+  if(pass.length >= 8){
+    $('.error-rel_password').text('');
+    $(this).css({"border": "2px solid #F3F3F3", "color": "#9EA5AB"});
+  }else{
+    $('.error-rel_password').text('Wachtwoord zou moeten minimaal 8 karakters');
+    $(this).css({"border": "2px solid #D17181", "color": "#D17181"});
+    $("#register_action_btn").attr('disabled','disabled');
+  }
+});
+
+
+$("#confirm_password").bind('blur keyup change click', function(){
+  $("#register_action_btn").prop("disabled",false);
+    var pass = $('#re_password').val();
+    var confpass = $(this).val();
+    //check the strings
+    if(pass == confpass){
+    //if both are same remove the error and allow to submit
+    $('.error-confirm_password').text('');
+    $(this).css({"border": "2px solid #F3F3F3", "color": "#9EA5AB"});
+    $("#register_action_btn").prop("disabled",false);
+    }else{
+    //if not matching show error and not allow to submit
+    $('.error-confirm_password').text('Wachtwoord komt niet overeen');
+    $(this).css({"border": "2px solid #D17181", "color": "#D17181"});
+    $("#register_action_btn").prop("disabled",true);
+    }
+});
+
+/* Checkout field show/hide */
+if ($("#billing_order_type_Zakelijk").is(":checked")) {
+    $('#billing_company_field').addClass('show-company');
+    $('#vat_number_field').addClass('show-vat_number');
+    $('#billing_reference_field').addClass('show-reference');
+}
+$("#billing_order_type_Zakelijk").on('change', function(){
+    if ($(this).is(":checked")) {
+        $('#billing_company_field').addClass('show-company');
+        $('#vat_number_field').addClass('show-vat_number');
+        $('#billing_reference_field').addClass('show-reference');
+    }
+});
+$("#billing_order_type_Particulier").on('change', function(){
+    if ($(this).is(":checked")) {
+        $('#billing_company_field').removeClass('show-company');
+        $('#vat_number_field').removeClass('show-vat_number');
+        $('#billing_reference_field').removeClass('show-reference');
+    }
+});
 
 
     new WOW().init();
