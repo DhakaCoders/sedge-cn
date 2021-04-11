d0282befb8d621743c40bd720130470b7f35736e
Rannojit <cserannojitsarkar22@gmail.com>
2021-04-11 11:59:45 +0600

    .

--- themes/sedge/page.php
+++ themes/sedge/page.php
@@ -3,7 +3,7 @@
   <?php if(is_account_page()):?>
   	<div class="container">
   		<div class="row">
-  			<div class="col-xs-12">
+  			<div class="col-md-12">
   				<article class="default-page-con">
   					<?php the_content(); ?>
   				</article>
