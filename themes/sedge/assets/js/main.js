b0e16cc08daabfb288259183980dbf4a8167e4a5
Foridul Islam <foridulsumon@gmail.com>
2021-04-16 02:48:58 +0600

    gu

--- themes/sedge/assets/js/main.js
+++ themes/sedge/assets/js/main.js
@@ -7,30 +7,6 @@ var windowWidth = $(window).width();
 $('.navbar-toggle').on('click', function(){
 	$('#mobile-nav').slideToggle(300);
 });
-	
-  
-//matchHeightCol
-if($('.mHc').length){
-  $('.mHc').matchHeight();
-};
-if($('.mHc1').length){
-  $('.mHc1').matchHeight();
-};
-if($('.mHc2').length){
-  $('.mHc2').matchHeight();
-};
-if($('.mHc3').length){
-  $('.mHc3').matchHeight();
-};
-if($('.mHc4').length){
-  $('.mHc4').matchHeight();
-};
-if($('.mHc5').length){
-  $('.mHc5').matchHeight();
-};
-if($('.mHc6').length){
-  $('.mHc6').matchHeight();
-};
 
 
 //$('[data-toggle="tooltip"]').tooltip();
@@ -449,8 +425,52 @@ $("#billing_order_type_Particulier").on('change', function(){
     }
 });
 
-
-    new WOW().init();
+//matchHeightCol
+if($('.mHc').length){
+  $('.mHc').matchHeight();
+};
+if($('.mHc1').length){
+  $('.mHc1').matchHeight();
+};
+if($('.mHc2').length){
+  $('.mHc2').matchHeight();
+};
+if($('.mHc3').length){
+  $('.mHc3').matchHeight();
+};
+if($('.mHc4').length){
+  $('.mHc4').matchHeight();
+};
+if($('.mHc5').length){
+  $('.mHc5').matchHeight();
+};
+if($('.mHc6').length){
+  $('.mHc6').matchHeight();
+};
+$(window).load(function() {
+//matchHeightCol
+  if($('.mHc').length){
+    $('.mHc').matchHeight();
+  };
+  if($('.mHc1').length){
+    $('.mHc1').matchHeight();
+  };
+  if($('.mHc2').length){
+    $('.mHc2').matchHeight();
+  };
+  if($('.mHc3').length){
+    $('.mHc3').matchHeight();
+  };
+  if($('.mHc4').length){
+    $('.mHc4').matchHeight();
+  };
+  if($('.mHc5').length){
+    $('.mHc5').matchHeight();
+  };
+  if($('.mHc6').length){
+    $('.mHc6').matchHeight();
+  };
+});
 
 })(jQuery);
 
