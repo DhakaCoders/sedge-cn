deebbc6a80bc29939d4236b538c7c3db9e61b8ee
azizulsoftsw <azizul.softsw@gmail.com>
2021-04-17 17:40:09 +0600

    Update xoo-cp-content.php

--- plugins/cbv-addet-to-cart-popup-wc/templates/xoo-cp-content.php
+++ plugins/cbv-addet-to-cart-popup-wc/templates/xoo-cp-content.php
@@ -105,26 +105,22 @@ $pattern   = apply_filters( 'woocommerce_quantity_input_pattern', has_filter( 'w
     </div>
 
   </div>
-			</div>
-			<div class="xoo-cp-pdetails-col-2">
-  			<div class="product-order-btn">
-          <div class="product-order-btn-top">
-            <a class="fl-btn" href="<?php echo wc_get_cart_url(); ;?>">
-              <span><?php _e('Verder naar bestellen','added-to-cart-popup-woocommerce'); ?></span>
-            </a>
-          </div>
-  				<div class="popUp-button-bottom">
-            <small>of</small> 
-            <a class="fl-btn continue-shopping-btn xoo-cp-close xcp-btn" href="<?php echo get_permalink(get_option( 'woocommerce_shop_page_id' ));?>">
-               <span> <?php _e('Verder winkelen','added-to-cart-popup-woocommerce'); ?></span>
-              </a>
-          </div>
-  			</div>
-			</div>
-		</div>
-
 	</div>
-</div>
+	<div class="xoo-cp-pdetails-col-2">
+		<div class="product-order-btn">
+      <div class="product-order-btn-top">
+        <a class="fl-btn" href="<?php echo wc_get_cart_url(); ;?>">
+          <span><?php _e('Verder naar bestellen','added-to-cart-popup-woocommerce'); ?></span>
+        </a>
+      </div>
+			<div class="popUp-button-bottom">
+        <small>of</small> 
+        <a class="fl-btn continue-shopping-btn xoo-cp-close xcp-btn" href="<?php echo get_permalink(get_option( 'woocommerce_shop_page_id' ));?>">
+           <span> <?php _e('Verder winkelen','added-to-cart-popup-woocommerce'); ?></span>
+          </a>
+      </div>
+		</div>
+	</div>
 <?php 
   $terms = get_the_terms($product_id, 'product_cat');
   $slugs = array();
